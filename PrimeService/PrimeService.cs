81dc2d8da5758e0f96101f8295df8c5e445ff903
Garrett Maring <garrett.maring@gmail.com>
2022-09-27 23:26:09 -0400

    Fix prime

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,13 +4,19 @@ namespace Prime.Services
 {
     public class PrimeService
     {
-        public bool IsPrime(int candidate)
+        public bool IsPrime(int number)
         {
-            if (candidate == 1)
-            {
-                return false;
-            }
-            throw new NotImplementedException("Not fully implemented.");
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
